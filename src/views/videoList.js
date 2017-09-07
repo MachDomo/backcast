552fab22ce684aca10a6c67bd02b252df22ef4d9
MachDomo <dominic.c.ma@gmail.com>
2017-09-06 17:10:24 -0700

    "add video list entry view"

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -10,13 +10,17 @@ var VideoListView = Backbone.View.extend({
     this.$el.html(this.template());
     // this.collection.forEach(this.renderEntry, this);
     //console.log(this.$el)
+    //loop through this.videos
+    this.collection.forEach(this.renderEntry, this);
+    //for each video
+    //this.renderEntry(video)
     return this;
   },
 
-  // renderEntry: function(entry) {
-  //   let videoView = new VideoListEntryView({model: entry});
-  //   this.$el.append(videoView.render());
-  // },
+  renderEntry: function(entry) {
+    let videoView = new VideoListEntryView({model: entry});
+    this.$el.append(videoView.render());
+  },
 
   template: templateURL('src/templates/videoList.html')
 
